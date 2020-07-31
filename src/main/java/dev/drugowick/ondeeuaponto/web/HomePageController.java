ebe51640f241907310428a4110e8ab1d97103bcd
Bruno Drugowick <bruno.drugowick@gmail.com>
2020-07-31 00:45:08 -0300

    Adiciona integraçao com o Google.

--- src/main/java/dev/drugowick/ondeeuaponto/web/HomePageController.java
+++ src/main/java/dev/drugowick/ondeeuaponto/web/HomePageController.java
@@ -1,18 +1,20 @@
 package dev.drugowick.ondeeuaponto.web;
 
 import dev.drugowick.ondeeuaponto.domain.client.jira.JiraClient;
-import dev.drugowick.ondeeuaponto.domain.client.jira.JiraIssueModel;
 import dev.drugowick.ondeeuaponto.domain.client.jira.JiraIssuesModel;
 import dev.drugowick.ondeeuaponto.domain.model.JiraIssue;
 import dev.drugowick.ondeeuaponto.domain.repository.JiraIssueRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -31,23 +33,45 @@ public class HomePageController {
     @Value("${jira.username}")
     String jiraUsername;
 
+    @Value("${jira.baseUrl}")
+    String jiraBaseUrl;
+
+    @ModelAttribute("jiraBaseUrl")
+    public String jiraBaseUrl() {
+        return jiraBaseUrl;
+    }
+
+    @ModelAttribute("username")
+    public String username(Principal principal) {
+        if (null != principal) return principal.getName();
+        return null;
+    }
+
     @RequestMapping
-    public String index(Model model) {
+    public String index(Model model, Principal principal) {
+        // TODO remove this and properly configure OAuth for the domain
+        if (notIcaro(principal)) return "redirect:/logout";
+
         model.addAttribute("issues", jiraIssueRepository.findAll());
         return "index";
     }
 
     @RequestMapping("/search")
-    public String search(@RequestParam("query") String query, Model model) {
-        if (query.isBlank()) return index(model);
+    public String search(@RequestParam("query") String query, Model model, Principal principal) {
+        // TODO remove this and properly configure OAuth for the domain
+        if (notIcaro(principal)) return "redirect:/logout";
 
-        log.info("User '" + jiraUsername + "' starting search for '" + query + "'; max results from Jira: " + jiraMaxResults);
+        if (query.isBlank()) return index(model, principal);
+
+        String twikiName = getTwikiName(principal);
+
+        log.info("User '" + twikiName + "' starting search for '" + query + "'; max results from Jira: " + jiraMaxResults);
 
         // TODO Create a meaningful service layer and move lots of stuff from here to there
         List<JiraIssue> allByDescriptionContaining = jiraIssueRepository.findAllByDescriptionContaining(query);
 
         final JiraIssuesModel jiraIssuesModel = jiraClient.assignedTo(
-                "assignee = " + jiraUsername +  " and (description ~ \"" + query + "\" OR summary ~ \"" + query + "\")" +
+                "assignee = " + twikiName +  " and (description ~ \"" + query + "\" OR summary ~ \"" + query + "\")" +
                 "and statusCategory not in (Done)", jiraMaxResults);
 
         jiraIssuesModel.getIssues().forEach(jiraIssueModel -> {
@@ -81,5 +105,27 @@ public class HomePageController {
         model.addAttribute("query", query);
 
         return "index";
+    }
+
+    private boolean notIcaro(Principal principal) {
+        OAuth2AuthenticationToken user = (OAuth2AuthenticationToken) principal;
+        String domain = user.getPrincipal().getAttributes().get("email").toString().split("@")[1];
+
+        if (domain.equals("icaro.com.br")) return false;
+
+        return true;
+    }
+
+    // TODO Proper user administration and information gathering to the database. Not in the controller.
+    private String getTwikiName(Principal principal) {
+        OAuth2AuthenticationToken user = (OAuth2AuthenticationToken) principal;
+        String twikiName = user.getPrincipal().getAttributes().get("email").toString().split("@")[0];
+        twikiName = twikiName.substring(0,1).toUpperCase() + twikiName.substring(1);
+        final int indexOfDot = twikiName.indexOf(".");
+        twikiName = twikiName.substring(0, indexOfDot)
+                + twikiName.substring(indexOfDot + 1, indexOfDot + 2).toUpperCase()
+                + twikiName.substring(indexOfDot + 2);
+        twikiName = twikiName.replace(".", "");
+        return twikiName;
     }
 }
